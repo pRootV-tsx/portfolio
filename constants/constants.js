b86b82aeb36adcdc60e8e0122111f55f262de77e
Pruthviraj Chauhan <yrgdstudios@gmail.com>
2023-11-16 10:33:13 +0530

    constant file clean, readme file update

--- constants/constants.js
+++ constants/constants.js
@@ -1,3 +1,61 @@
+/* 
+This constants.js file contains
+- navigation -- navigation links (title, link)
+- views -- views on youtube, github, medium (title, icon, views)
+- about -- about me (title, content)
+- connect -- social media links (github, twitter, youtube, dribbble, instagram, linkedin)
+- recentProjects -- recent projects (5)
+- myTools -- my tools (hardware, software, other)
+- hardware -- mytools - hardware (laptop, phone, tablet, headphones)
+- software -- mytools - software (adobe, figma, jira, notion, spotify)
+- other -- mytools - other (pen, paper, sleep, walk)
+- aboutImages -- my images for about page (profile, laptop, friends, music)
+*/
+
+import {
+	SiAdobe,
+	SiFigma,
+	SiJira,
+	SiVisualstudiocode,
+	SiNotion,
+	SiSpotify,
+} from "react-icons/si"
+
+import {
+	FaGithub,
+	FaGlobe,
+	FaMedium,
+	FaWalking,
+	FaYoutube,
+} from "react-icons/fa"
+
+import { CrumpledPaperIcon } from "@radix-ui/react-icons"
+
+import dreamdream from "@/public/projectImages/dreamdream.png"
+import ovrcode from "@/public/projectImages/ovrcode.jpg"
+import gFinder from "@/public/projectImages/gfinder.png"
+import designChallenge from "@/public/projectImages/dailydesign.png"
+import typography from "@/public/projectImages/typographyposters.png"
+import {
+	CoffeeIcon,
+	DribbbleIcon,
+	GithubIcon,
+	Headphones,
+	InstagramIcon,
+	Laptop2,
+	Linkedin,
+	Pen,
+	Phone,
+	Tablet,
+	TwitterIcon,
+	YoutubeIcon,
+} from "lucide-react"
+
+import profile from "@/public/profile.png"
+import laptop from "@/public/laptop.jpeg"
+import music from "@/public/music.png"
+import friends from "@/public/friends.jpeg"
+
 const navigation = [
 	{
 		id: 1,
@@ -21,64 +79,28 @@ const navigation = [
 	},
 ]
 
-// Github, YouTube, Blogs views js
-
-const { Youtube, Github, LibraryBig } = require("lucide-react")
-
-import yuya from "@/public/projectImages/yuya.png"
-import dreamdream from "@/public/projectImages/dreamdream.png"
-import ovrcode from "@/public/projectImages/ovrcode.jpg"
-import gFinder from "@/public/projectImages/gfinder.png"
-import designChallenge from "@/public/projectImages/dailydesign.png"
-import typography from "@/public/projectImages/typographyposters.png"
-import {
-	BatteryMedium,
-	CoffeeIcon,
-	DribbbleIcon,
-	GithubIcon,
-	Headphones,
-	InstagramIcon,
-	Laptop,
-	Linkedin,
-	Pen,
-	Phone,
-	Tablet,
-	Tangent,
-	TwitchIcon,
-	TwitterIcon,
-	YoutubeIcon,
-} from "lucide-react"
-import { GitHubLogoIcon } from "@radix-ui/react-icons"
-
 const views = [
 	{
 		id: 1,
-		icon: <Github />,
+		icon: <FaGithub className="w-6 h-6 " />,
 		title: "Github",
-		views: "1.2k",
+		views: "N/A",
 	},
 	{
 		id: 2,
-		icon: <Youtube />,
+		icon: <FaYoutube className="w-6  h-6" />,
 		title: "Youtube",
-		views: "5.2k",
+		views: "7.5k",
 	},
 	{
 		id: 3,
-		icon: <LibraryBig />,
+		icon: <FaMedium className="w-6  h-6" />,
 		title: "Blogs",
-		views: "238",
+		views: "120",
 	},
 ]
 
-// Ui Design projects
-
-// totalDesign projects == OvrCode Branding, LogoFolio 2022-23, Learniverse Webdesignm, Typography Posters, 365 Design Challenge, Dream Dream Web Design, Yuya Mei personal brand, Github Finder, Zapiya Web Designm , Teleprompter app design
-
-// totalDevelopment projects == Coming Soon [Discord Clone, DevOverFlow , GithubFinder]
-
 const designProjects = [
-	/* id, name, image, link, isUnderConstruction, simplebrief, link, */
 	{
 		id: "des001",
 		name: "OvrCode Branding",
@@ -93,7 +115,7 @@ const designProjects = [
 		name: "Dream Dream Web Design",
 		image: dreamdream,
 		link: "https://www.figma.com/file/zNMlVCOOy4vlZmnSGWZPuv/DreamDream-Website?type=design&mode=design&t=W7KqCyGuOoQUFPMe-1",
-		isUnderConstruction: false, //then add coming soon text
+		isUnderConstruction: false,
 		simplebrief:
 			"Dream Dream is a web design for a team. I used a vibrant color palette and a bold logo.",
 	},
@@ -111,7 +133,7 @@ const designProjects = [
 		name: "Typography Posters",
 		image: typography,
 		link: "https://www.behance.net/gallery/151705513/Custom-Typography-Vol01",
-		isUnderConstruction: false, //then add coming soon text
+		isUnderConstruction: false,
 		simplebrief:
 			"Typography posters made with Adobe Illustrator and Adobe Photoshop.",
 	},
@@ -120,7 +142,7 @@ const designProjects = [
 		name: "365 Design Challenge",
 		image: designChallenge,
 		link: "https://www.behance.net/gallery/156534597/Poster-Project-02-Poster-Experiments",
-		isUnderConstruction: false, //then add coming soon text
+		isUnderConstruction: false,
 		simplebrief:
 			"365 Design Challenge is a personal project where I design something everyday for 365 days.",
 	},
@@ -129,7 +151,7 @@ const designProjects = [
 		name: "LogoFolio 2022-23",
 		image: ovrcode,
 		link: "",
-		isUnderConstruction: true, //then add coming soon text
+		isUnderConstruction: true,
 		simplebrief:
 			"All of my logos in one place. I will be updating this project every year with new logos.",
 	},
@@ -138,7 +160,7 @@ const designProjects = [
 		name: "Yuya Mei personal brand",
 		image: ovrcode,
 		link: "",
-		isUnderConstruction: true, //then add coming soon text
+		isUnderConstruction: true,
 		simplebrief:
 			"Yuya Mei is a personal brand for a friend. I used a simple color palette and a simple logo.",
 	},
@@ -148,7 +170,7 @@ const designProjects = [
 		name: "Learniverse Webdesign",
 		image: gFinder,
 		link: "",
-		isUnderConstruction: true, //then add coming soon text
+		isUnderConstruction: true,
 		simplebrief:
 			"Learniverse is a learning platform where you can learn anything. This is a web design for the platform.",
 	},
@@ -157,104 +179,84 @@ const designProjects = [
 		name: "Zapiya Web Design",
 		image: ovrcode,
 		link: "",
-		isUnderConstruction: true, //then add coming soon text
+		isUnderConstruction: true,
 		simplebrief: "Still talking with the client about the project",
 	},
 ]
 
-//  recentProjects  gets frist 5 projects from designProjects array
 const recentProjects = designProjects.slice(0, 5)
 
-// Tools all tools
-
-import {
-	SiAdobe,
-	SiFigma,
-	SiJira,
-	SiVisualstudiocode,
-	SiNotion,
-	SiSpotify,
-	SiPaperspace,
-} from "react-icons/si"
-
-import { FaGlobe, FaWalking } from "react-icons/fa"
+const iconClassname = "w-8 h-8 text-2xl text-zinc-500 dark:text-zinc-400"
 
 const myTools = [
-	// Hardware
 	{
 		category: "hardware",
-		tools:
-			//Apple Macbook Pro 2020, Redmi Note 9 Pro, iPad Air , Boat Airdopes
-			[
-				{
-					tool: "Apple Macbook Pro 2020 16inch",
-					icon: <Laptop className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />,
-					info: "Where sleek design meets coding dreams, powering creativity with every keystroke. Unleash my code in style!",
-				},
-				{
-					tool: "Redmi Note 9 Pro",
-					icon: <Phone className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />,
-					info: `My pocket powerhouse, capturing moments with flair and running my apps at the speed of my dynamic life. It's not just a phone; it's my creative companion on the go!`,
-				},
-				{
-					tool: "iPad Air",
-					icon: <Tablet className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />,
-					info: `Meet my creativity wingman, rocking sleek vibes and packing the mojo to turn my ideas into pure awesome.`,
-				},
-				{
-					tool: "Boat Airdopes 148",
-					icon: (
-						<Headphones className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />
-					),
-					info: ` My audio sidekick, grooving to the beats of my day and cutting the cords on boring moments. `,
-				},
-			],
+		tools: [
+			{
+				tool: "Apple Macbook Pro 2020 16inch",
+				icon: <Laptop2 width={8} height={8} className={iconClassname} />,
+				info: "Where sleek design meets coding dreams, powering creativity with every keystroke. Unleash my code in style!",
+			},
+			{
+				tool: "Redmi Note 9 Pro",
+				icon: <Phone width={8} height={8} className={iconClassname} />,
+				info: `My pocket powerhouse, capturing moments with flair and running my apps at the speed of my dynamic life. It's not just a phone; it's my creative companion on the go!`,
+			},
+			{
+				tool: "iPad Air",
+				icon: <Tablet width={8} height={8} className={iconClassname} />,
+				info: `Meet my creativity wingman, rocking sleek vibes and packing the mojo to turn my ideas into pure awesome.`,
+			},
+			{
+				tool: "Boat Airdopes 148",
+				icon: <Headphones width={8} height={8} className={iconClassname} />,
+				info: ` My audio sidekick, grooving to the beats of my day and cutting the cords on boring moments. `,
+			},
+		],
 	},
 	{
 		category: "Software",
 		tools: [
 			{
 				tool: "Adobe CC",
-				icon: <SiAdobe className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />,
+				icon: <SiAdobe width={8} height={8} className={iconClassname} />,
 				info: "Adobe Photoshop, Adobe Illustrator, Adobe XD, Adobe After Effects, Adobe Premiere Pro, Adobe Lightroom",
 			},
 			{
 				tool: "Figma & FigJam",
-				icon: <SiFigma className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />,
+				icon: <SiFigma width={8} height={8} className={iconClassname} />,
 				info: "Figma for UI/UX Design, FigJam for Brainstorming",
 			},
 			{
 				tool: "Jira",
-				icon: <SiJira className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />,
+				icon: <SiJira width={8} height={8} className={iconClassname} />,
 				info: "Jira for project management",
 			},
 			{
 				tool: "Procreate",
-				icon: <Pen className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />,
+				icon: <Pen width={8} height={8} className={iconClassname} />,
 				info: "Procreate for logo sketches and random doodles",
 			},
 			{
 				tool: "VS Code",
 				icon: (
-					<SiVisualstudiocode className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />
+					<SiVisualstudiocode width={8} height={8} className={iconClassname} />
 				),
 				info: "VS Code for coding and for my life :) ",
 			},
 			{
 				tool: "Arc Browser",
-				icon: <FaGlobe className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />,
+				icon: <FaGlobe width={8} height={8} className={iconClassname} />,
 				info: " My digital maestro, orchestrating a symphony of seamless web journeys with style. It's not just a browser; ",
 			},
 			{
 				tool: "Notion",
-				icon: <SiNotion className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />,
+				icon: <SiNotion width={8} height={8} className={iconClassname} />,
 				info: "Notion for note taking and planning",
 			},
 			{
 				tool: "Spotify",
-				icon: (
-					<SiSpotify className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />
-				),
+				icon: <SiSpotify width={8} height={8} className={iconClassname} />,
 				info: `It's my daily dose of sonic delight! Spinning tracks that match my vibe and transforming routines into a musical realm`,
 			},
 		],
@@ -265,23 +267,17 @@ const myTools = [
 		tools: [
 			{
 				tool: "Pen & Paper",
-				icon: (
-					<SiPaperspace className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />
-				),
+				icon: <CrumpledPaperIcon className={iconClassname} />,
 				info: "Pen & Paper for sketching and writing down ideas",
 			},
 			{
 				tool: "Coffee or Tea",
-				icon: (
-					<CoffeeIcon className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />
-				),
+				icon: <CoffeeIcon className={iconClassname} />,
 				info: "Coffee & Tea for energy and continuous work",
 			},
 			{
 				tool: "Sleep & Walk",
-				icon: (
-					<FaWalking className="w-8 h-8 text-zinc-500 dark:text-zinc-400" />
-				),
+				icon: <FaWalking className={iconClassname} />,
 				info: "Sleep & Walk for fixing bugs",
 			},
 		],
@@ -344,12 +340,6 @@ const hardware = myTools[0].tools
 
 const software = myTools[1].tools
 const other = myTools[2].tools
-
-import profile from "@/public/profile.png"
-import laptop from "@/public/laptop.jpeg"
-import pruthvi from "@/public/profile.png"
-import music from "@/public/music.png"
-import friends from "@/public/friends.jpeg"
 
 const aboutImages = [
 	{
